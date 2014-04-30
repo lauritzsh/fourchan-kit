1ecae850641bca52e7c31601f306994be8ff1969
lauritzsh <mail@lauritz.me>
2014-04-30 11:26:15 +0200

    Fetch 4chan board names

--- spec/fourchan_kit_spec.rb
+++ spec/fourchan_kit_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
+describe FourchanKit, :vcr do
+  it 'should be able to get an array of board names' do
+    VCR.use_cassette 'api/boards' do
+      boards = FourchanKit.fourchan_boards
+      boards.length.should == 63
+      boards.first.should == '3'
+      boards.last.should == 'y'
+    end
+  end
+end
+
 describe FourchanKit::API, :vcr do
   it 'should be able to get info for all boards' do
     VCR.use_cassette 'api/boards' do
