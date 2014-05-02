614ffad38dec58d80e0df666ebcfa6e655f67a06
lauritzsh <mail@lauritz.me>
2014-05-02 21:20:51 +0200

    Make use of new test data and automatically name cassettes

--- spec/fourchan_kit_api_spec.rb
+++ spec/fourchan_kit_api_spec.rb
@@ -2,44 +2,34 @@ require 'spec_helper'
 
 describe FourchanKit::API, :vcr do
   it 'should be able to get info for all boards' do
-    VCR.use_cassette 'api/boards' do
-      boards = FourchanKit::API.get_boards
-      boards.length.should == 63
-    end
+    boards = FourchanKit::API.get_boards
+    boards.length.should == 63
   end
 
   it 'should be able to get the catalog for a board' do
-    VCR.use_cassette 'api/catalog' do
-      catalog = FourchanKit::API.get_catalog 'g'
-      catalog.length.should == 10
-      catalog.first['page'].should == 1
-      catalog.first['threads'].first['no'] == 39894014
-    end
+    catalog = FourchanKit::API.get_catalog 'g'
+    catalog.length.should == 10
+    catalog.first['page'].should == 1
+    catalog.first['threads'].first['no'] == 39894014
   end
 
   it 'should be able to get the threads for a board' do
-    VCR.use_cassette 'api/threads' do
-      threads = FourchanKit::API.get_threads 'g'
-      threads.length.should == 10
-      threads.first['page'].should == 1
-      threads.first['threads'].first['no'].should == 39894014
-    end
+    threads = FourchanKit::API.get_threads 'g'
+    threads.length.should == 10
+    threads.first['page'].should == 1
+    threads.first['threads'].first['no'].should == 39894014
   end
 
   it 'should be able to get the posts from a thread' do
-    VCR.use_cassette 'api/posts' do
-      posts = FourchanKit::API.get_thread 'g', 41658861
-      posts.length.should == 69
-      posts.first['no'].should == 41658861
-      posts.last['no'].should == 41665533
-    end
+    posts = FourchanKit::API.get_thread 'g', 41706090
+    posts.length.should == 6
+    posts.first['no'].should == 41706090
+    posts.last['no'].should == 41706626
   end
 
   it 'should be able to get threads from a page' do
-    VCR.use_cassette 'api/page' do
-      posts = FourchanKit::API.get_page 'g', 1
-      posts.length.should == 15
-      posts.first['posts'].first['no'].should == 39894014
-    end
+    posts = FourchanKit::API.get_page 'g', 1
+    posts.length.should == 15
+    posts.first['posts'].first['no'].should == 39894014
   end
 end
