080e39dc53a15b80f88917f92aebeb114badafb7
lauritzsh <mail@lauritz.me>
2014-05-08 22:15:14 +0200

    Better specs for board

--- spec/fourchan_kit_board_spec.rb
+++ spec/fourchan_kit_board_spec.rb
@@ -2,27 +2,25 @@ require 'spec_helper'
 
 describe Fourchan::Kit::Board, :vcr do
 
-  before(:each) do
-    @board = Fourchan::Kit::Board.new 'g'
-  end
+  let(:board) { Fourchan::Kit::Board.new('g') }
 
   it 'should have 10 pages' do
-    @board.board.length.should == 10
+    expect(board.catalog.length).to eql(10)
   end
 
   it 'and have 15 threads per page' do
-    @board.threads(1).length.should == 15
+    expect(board).to have(15).threads(1)
   end
 
   it 'and should have a total of 150 threads' do
-    @board.all_threads.length.should == 150
+    expect(board.all_threads.length).to eql(150)
   end
 
   it 'should have 908 posts on the first page' do
-    @board.posts.length.should == 908
+    expect(board).to have(908).posts
   end
 
   it 'and a total of 7685 posts' do
-    @board.all_posts.length.should == 7685
+    expect(board.all_posts.length).to eql(7685)
   end
 end
